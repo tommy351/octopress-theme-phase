088633220d0722e311d63b2c586bb271979b46df
Tommy Chen <tommy351@gmail.com>
2012-04-29 18:33:37 +0800

    external.js與fancy box衝突

--- source/javascripts/external.js
+++ source/javascripts/external.js
@@ -2,12 +2,14 @@
 	var host = location.host;
 
 	$('body').on('click', 'a', function(e){
-		var href = $(this).attr('href'),
-			link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
+		if (!$(this).hasClass('fancybox')){
+			var href = $(this).attr('href'),
+				link = href.replace(/(https?:\/\/)(.*)\/(.*)/, '$2');
 
-		if (href.match('https?') && link != host){
-			window.open(href);
-			e.preventDefault();
+			if (href.match('https?') && link != host){
+				window.open(href);
+				e.preventDefault();
+			}
 		}
 	});
 })(jQuery);
